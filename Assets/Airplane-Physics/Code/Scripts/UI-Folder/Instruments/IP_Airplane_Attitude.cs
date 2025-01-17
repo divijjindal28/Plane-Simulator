87c0aff9e958a023b4c07a7c98682e2b6dd12eb5
divijjindalnevrio <divij@nevrio.tech>
2025-01-17 16:50:33 +0530

    airplane-ui-set

---
+++ Assets/Airplane-Physics/Code/Scripts/UI-Folder/Instruments/IP_Airplane_Attitude.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using IndiePixel;
+using UnityEngine;
+
+namespace IndiePixel {
+    public class IP_Airplane_Attitude : MonoBehaviour, IAirplaneUI
+    {
+        #region Variables
+        [Header("Attitude Indicator Properties")]
+        public IP_Airplane_Controller airplane;
+        public RectTransform bgRect;
+        public RectTransform arrowRect;
+        
+        #endregion
+
+        public const float mphtoKnots = 0.868976f;
+
+        #region Interface Methods
+        public void HandleAirplaneUI()
+        {
+            Debug.Log("IP_Airplane_Tachometer HandleAirplaneUI RPM : WORKING");
+            if (airplane && arrowRect)
+            {
+                float bankAngle = Vector3.Dot(airplane.transform.right, Vector3.up) * Mathf.Rad2Deg;
+                float pitchAngle = Vector3.Dot(airplane.transform.forward, Vector3.up) * Mathf.Rad2Deg;
+
+
+                if (bgRect) {
+                    Quaternion bankRot = Quaternion.Euler(0, 0, bankAngle);
+                    bgRect.transform.rotation = bankRot;
+
+                    Vector3 wantedPosition = new Vector3(0f, -pitchAngle , 0f);
+                    bgRect.anchoredPosition = wantedPosition;
+                    if (arrowRect) {
+                        arrowRect.transform.rotation = bankRot;
+                    }
+                }
+
+            }
+            throw new System.NotImplementedException();
+        }
+        #endregion
+    }
+}
+
